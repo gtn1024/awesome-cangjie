055a3ac679fa1e68bd429ee46ac35978f164c42d
Taoning Ge <gtn1024@foxmail.com>
2025-12-31 11:15:11 +0800

    chore: add google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 export default defineConfig({
   plugins: [
     vue(),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-FJ2JVMY164',
+      },
+    }),
   ],
 })
